64604f7336bf66080356ef4f2e32186c8063fe69
Chouhan143 <sunilchouhan773@gmail.com>
2024-03-12 19:10:16 +0530

    logiscreen add

--- Src/Auth/LoginScreen.js
+++ Src/Auth/LoginScreen.js
@@ -1,17 +1,102 @@
-import {View, Text,Image} from 'react-native';
+import {View, Text, Image, TextInput} from 'react-native';
 import React from 'react';
 import Font5 from 'react-native-vector-icons/FontAwesome5';
 import loginStyles from './loginStyle';
+import {
+  responsiveFontSize,
+  responsiveHeight,
+  responsiveWidth,
+} from 'react-native-responsive-dimensions';
 
 const LoginScreen = () => {
   return (
     <View style={loginStyles.container}>
       <View style={loginStyles.topContainer}>
-        <Image source={require("../assets/Rectangle1.png")}/>
-        <Image source={require("../assets/Rectangle1.png")}/>
+        <Image source={require('../assets/Rectangle1.png')} />
+        <Image
+          source={require('../assets/Rectangle_5.png')}
+          style={loginStyles.ImageStyle}
+        />
       </View>
-      <View style={loginStyles.inputFiledContainer}></View>
-      <View style={loginStyles.bottomContainer}></View>
+      <View style={loginStyles.inputFiledContainer}>
+        <View style={{marginHorizontal: responsiveWidth(5)}}>
+          <Text style={loginStyles.LoginFont}>Login</Text>
+          <View style={{marginTop: responsiveHeight(1)}}>
+            <Text style={loginStyles.textInputeText}>Email</Text>
+            <TextInput style={loginStyles.textInpute} />
+            <Text
+              style={[
+                loginStyles.textInputeText,
+                {marginTop: responsiveHeight(1.5)},
+              ]}>
+              Password
+            </Text>
+            <TextInput style={loginStyles.textInpute} />
+            {/* forgote password ui  */}
+            <View
+              style={{
+                alignItems: 'flex-end',
+                marginRight: responsiveWidth(9),
+                marginTop: responsiveFontSize(1),
+              }}>
+              <Text style={loginStyles.textInputeText}>Forgot Password?</Text>
+            </View>
+            <View style={{flexDirection: 'row', gap: responsiveWidth(3)}}>
+              <View style={loginStyles.socialIcon}>
+                <Image
+                  source={require('../assets/google.png')}
+                  resizeMode="contain"
+                  style={loginStyles.IconStyle}
+                />
+              </View>
+              <View style={loginStyles.socialIcon}>
+                <Image
+                  source={require('../assets/facebook.png')}
+                  resizeMode="contain"
+                  style={loginStyles.IconStyle}
+                />
+              </View>
+              <View style={loginStyles.socialIcon}>
+                <Image
+                  source={require('../assets/apple.png')}
+                  resizeMode="contain"
+                  style={loginStyles.IconStyle}
+                />
+              </View>
+            </View>
+          </View>
+        </View>
+      </View>
+      <View style={loginStyles.bottomContainer}>
+        <Image source={require('../assets/Rectangle3.png')} />
+        <View
+          style={{
+            position: 'absolute',
+            bottom: responsiveHeight(2),
+            paddingHorizontal: responsiveWidth(3),
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'flex-end',
+          }}>
+          <View>
+            <Text style={[loginStyles.textInputeText, {color: '#fff'}]}>
+              New Here? Register
+            </Text>
+          </View>
+          <View
+            style={{
+              alignItems: 'flex-end',
+              paddingHorizontal: responsiveWidth(3.5),
+              paddingVertical: responsiveWidth(1.5),
+              borderWidth: responsiveWidth(0.3),
+              borderRadius: responsiveWidth(1),
+              borderColor: '#fff',
+              marginLeft: responsiveWidth(28),
+            }}>
+            <Text style={[loginStyles.LoginFont, {color: '#fff'}]}>Login</Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
--- Src/Auth/loginStyle.js
+++ Src/Auth/loginStyle.js
@@ -1,23 +1,64 @@
-import { StyleSheet } from "react-native";
-import { responsiveScreenHeight, responsiveScreenWidth } from "react-native-responsive-dimensions";
+import {StyleSheet} from 'react-native';
+import {
+  responsiveFontSize,
+  responsiveHeight,
+  responsiveScreenHeight,
+  responsiveScreenWidth,
+  responsiveWidth,
+} from 'react-native-responsive-dimensions';
 
 const loginStyles = StyleSheet.create({
-    container:{
-        width:responsiveScreenWidth(100),
-        height:responsiveScreenHeight(100),
-    },
-    topContainer:{
-        flex:3,
-        backgroundColor:"red"
-    },
-    inputFiledContainer:{
-        flex:2,
-        backgroundColor:'green'
-    },
-    bottomContainer:{
-        flex:1,
-        backgroundColor:"purple"
-    }
-})
+  container: {
+    width: responsiveScreenWidth(100),
+    height: responsiveScreenHeight(100),
+  },
+  topContainer: {
+    flex: 2,
+    backgroundColor: '#fff',
+  },
+  inputFiledContainer: {
+    flex: 2,
+  },
+  bottomContainer: {
+    flex: 1,
+    position: 'absolute',
+    bottom: responsiveHeight(8),
+  },
+  ImageStyle: {
+    position: 'absolute',
+    top: responsiveHeight(7),
+    zIndex: -10,
+  },
+  LoginFont: {
+    fontSize: responsiveFontSize(3.4),
+    fontWeight: '800',
+    color: '#2F80ED',
+  },
+  textInputeText: {
+    fontSize: responsiveFontSize(2),
+    fontWeight: '400',
+    color: '#2F80ED',
+  },
+  textInpute: {
+    width: responsiveWidth(80),
+    height: responsiveHeight(5),
+    backgroundColor: '#D9D9D9',
+    borderRadius: responsiveWidth(2),
+  },
+  socialIcon: {
+    backgroundColor: '#fff',
+    width: responsiveWidth(12),
+    height: responsiveWidth(12),
+    shadowColor: '#000',
+    elevation: 5,
+    borderRadius: responsiveWidth(1),
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  IconStyle: {
+    width: responsiveWidth(8),
+    height: responsiveWidth(8),
+  },
+});
 
-export default loginStyles
+export default loginStyles;
